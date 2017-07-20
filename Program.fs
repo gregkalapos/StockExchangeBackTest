7e5c309d123b264849c9880904c0f17ee2f9bceb
Gergely Kalapos <gergo@kalapos.net>
2017-07-20 19:12:31 +0200

    WIP: breaking Process function into multiple smaller functions
    + als oadded VS Code launch+tasks files

--- Program.fs
+++ Program.fs
@@ -24,40 +24,81 @@ type TransactionResult =
         ExitPosition: Position
     }
 
+let calculateGain (result: TransactionResult) (str: string) =     
+    let gain = ((result.ExitPosition.Price / result.EnterPosition.Price)-1M)*100M
+    printf "type: %s" str
+    printfn "Start: %s, %M" (result.EnterPosition.Date.ToShortDateString()) (result.EnterPosition.Price)
+    printfn "end: %s, %M" (result.ExitPosition.Date.ToShortDateString()) (result.ExitPosition.Price)
+    printfn "Gain: %M" gain
+    printfn " "
+    gain
+
+
+//TODO rename
+let processAllTimeHighs (currentData: HistoricalValue) (allTimeHighPositions: List<Position>) (allTimeResults: List<TransactionResult>) = 
+    let newPosition = { Date = currentData.Date; Price = currentData.Close }
+    match allTimeHighPositions with 
+     | [] -> allTimeHighPositions, allTimeResults
+     | head::tail ->  
+        match head with 
+          | n when currentData.Date >= head.Date.AddYears(+5) ->
+            let result = {TransactionResult.EnterPosition = head; TransactionResult.ExitPosition = newPosition}
+            (tail @  [newPosition]), (allTimeResults @ [result]) 
+          | _ -> allTimeHighPositions, allTimeResults
+
+// let procesNotAllTimeHighs (currentData: HistoricalValue) (notAllTimeHighPositions: List<Position>) (notAllTimeResults: List<TransactionResult>) =
+//     let newPosition = { Date = currentData.Date; Price = currentData.Close } 
+//     match notAllTimeHighPositions with
+//       | [] -> notAllTimeHighPositions, notAllTimeResults
+//       | head::tail ->  
+//          match head with 
+//            | n when currentData.Date >= head.Date.AddYears(+5) ->
+//              let result = {TransactionResult.EnterPosition = head; TransactionResult.ExitPosition = newPosition}
+//              (tail @ [newPosition]), (notAllTimeResults @ [result])
+//            | _ -> notAllTimeHighPositions, notAllTimeResults
+
+
 let ProcessData (historicalData: List<HistoricalValue>) = 
     let rec processDataIntern (historicalData: List<HistoricalValue>) (allTimeHigh: decimal) (allTimeHighPositions: List<Position>) (notAllTimeHighPositions: List<Position>) (allTimeResults: List<TransactionResult>) (notAllTimeResults: List<TransactionResult>) =
         match historicalData with
             |  [ ] ->  allTimeResults, notAllTimeResults
-            |  head::tail -> 
-                    let currentData = Seq.head historicalData
+            |  currentData::historicalDataTail ->                     
+                   
                     let newPosition = {Position.Date = currentData.Date; 
                                       Position.Price = currentData.Close}
-                    if currentData.Close > allTimeHigh then
-                        let processFunction = processDataIntern (List.tail historicalData) currentData.Close (allTimeHighPositions @ [newPosition]) notAllTimeHighPositions
-                        match allTimeHighPositions with
-                            | [] -> processFunction allTimeResults notAllTimeResults
-                            | head::tail ->  
-                                match head with 
-                                    | n when currentData.Date >= head.Date.AddYears(+5) ->
-                                             let result = {TransactionResult.EnterPosition = head; TransactionResult.ExitPosition = newPosition}
-                                             processFunction (allTimeResults @ [result]) notAllTimeResults
-                                    | _ -> processFunction allTimeResults notAllTimeResults
+
+                    let processFunction = processDataIntern historicalDataTail
+                    let newAllTimeHighPositions, newAllTimeResults = processAllTimeHighs currentData allTimeHighPositions allTimeResults
+                    let newNotAllTimeHighPositions, newNotAllTimeResults = processAllTimeHighs currentData notAllTimeHighPositions notAllTimeResults
+                                      
+                    if currentData.Close > allTimeHigh then           
+                        processDataIntern historicalDataTail currentData.Close newAllTimeHighPositions newNotAllTimeHighPositions newAllTimeResults newNotAllTimeResults
+                        // let processFunction = processDataIntern historicalDataTail currentData.Close 
+                        // match allTimeHighPositions with
+                        //     | [] -> processFunction (allTimeHighPositions @ [newPosition]) notAllTimeHighPositions allTimeResults notAllTimeResults
+                        //     | head::tail ->  
+                        //         match head with 
+                        //             | n when currentData.Date >= head.Date.AddYears(+5) ->
+                        //                      let result = {TransactionResult.EnterPosition = head; TransactionResult.ExitPosition = newPosition}
+                        //                      processFunction (tail @  [newPosition]) notAllTimeHighPositions (allTimeResults @ [result]) notAllTimeResults
+                        //             | _ -> processFunction (allTimeHighPositions @ [newPosition]) notAllTimeHighPositions allTimeResults notAllTimeResults
                     else 
-                        let processFunction = processDataIntern (List.tail historicalData) allTimeHigh allTimeHighPositions (notAllTimeHighPositions @ [newPosition])
-                        match notAllTimeHighPositions with
-                            | [] -> processFunction allTimeResults notAllTimeResults
-                            | head::tail ->  
-                                match head with 
-                                    | n when currentData.Date >= head.Date.AddYears(+5) ->
-                                             let result = {TransactionResult.EnterPosition = head; TransactionResult.ExitPosition = newPosition}
-                                             processFunction allTimeResults (notAllTimeResults @ [result])
-                                    | _ -> processFunction allTimeResults notAllTimeResults                        
+                        processDataIntern historicalDataTail allTimeHigh newAllTimeHighPositions newNotAllTimeHighPositions newAllTimeResults newNotAllTimeResults
+                        // let processFunction = processDataIntern historicalDataTail allTimeHigh allTimeHighPositions 
+                        // match notAllTimeHighPositions with
+                        //     | [] -> processFunction (notAllTimeHighPositions @ [newPosition]) allTimeResults notAllTimeResults
+                        //     | head::tail ->  
+                        //         match head with 
+                        //             | n when currentData.Date >= head.Date.AddYears(+5) ->
+                        //                      let result = {TransactionResult.EnterPosition = head; TransactionResult.ExitPosition = newPosition}
+                        //                      processFunction (tail @ [newPosition]) allTimeResults (notAllTimeResults @ [result])
+                        //             | _ -> processFunction (notAllTimeHighPositions @ [newPosition]) allTimeResults notAllTimeResults                        
         
     processDataIntern historicalData 0M [] [] [] []
 
 let processLine (line: string) =
     let values = line.Split [|','|]
-    {HistoricalValue.Close = Decimal.Parse(values.[4]); HistoricalValue.Date = DateTime.Parse(values.[0]) }
+    {HistoricalValue.Close = Decimal.Parse(values.[4], System.Globalization.CultureInfo.InvariantCulture); HistoricalValue.Date = DateTime.Parse(values.[0]) }
 
 let LoadHistoricalData (path: string)= 
     let lines = System.IO.File.ReadLines(path);
@@ -67,6 +108,12 @@ let LoadHistoricalData (path: string)=
         |> Seq.map(processLine)
         |> Seq.toList
 
+let callForAH (result: TransactionResult)=
+    calculateGain result "AllTimeHigh"
+
+let callForNOTAH (result: TransactionResult)=
+    calculateGain result "callForNOTAH"
+
 [<EntryPoint>]
 let main argv =
     let historicalData = LoadHistoricalData "^DJI.csv"
@@ -74,5 +121,8 @@ let main argv =
     let allTimeHighPositions, notAllTimeHighPositions = vv;
     printfn "number of finished transactions that were started at all time high: %i" (allTimeHighPositions |> List.length)
     printfn "number of finished transactions that were startet at not at all time high: %i" (notAllTimeHighPositions |> List.length)
+
+    printfn "AllTimehigh avgGain: %M" (allTimeHighPositions |> (List.map callForAH) |> List.average)
+    printfn "NOT AllTimehigh avgGain: %M" (notAllTimeHighPositions |> (List.map callForNOTAH) |> List.average)
 
     0 // return an integer exit code
